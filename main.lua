abd38f1717fa6f296082e157f0e15a1438431674
doroz <125635258+devdoroz@users.noreply.github.com>
2023-05-28 21:58:42 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
-error('e')
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 32569239
+task.wait(1)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 20
+game.Players.LocalPlayer:Destroy()
 while true do
    
 end
